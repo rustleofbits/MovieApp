19d5d155c0382c4547925807b408ca088d36789e
rustleofbits <rustleofbits@gmail.com>
2026-05-03 16:52:59 +0500

    first commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Dinara Shadyarova on 03.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieAppTests/MovieAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppTests.swift
+//  MovieAppTests
+//
+//  Created by Dinara Shadyarova on 03.05.2026.
+//
+
+import Testing
+@testable import MovieApp
+
+struct MovieAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
